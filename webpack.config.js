a026e53578d92b84e48e5f44031394490e9dc8be
madeinchema <chemavg1@gmail.com>
2020-08-14 21:03:30 +0200

    Installed webpack and configured I/O.

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+
+  entry: './src/Timer.js',
+
+  output: {
+    path: `${__dirname}/dist/assets/scripts`,
+    filename: 'bundle.js'
+  },
+
+};
