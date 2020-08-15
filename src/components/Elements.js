0f74001e5ccd6bf4e2882207e24b8b7d65b88d52
madeinchema <chemavg1@gmail.com>
2020-08-15 14:38:32 +0200

    Cleaned commented code.

--- src/components/Elements.js
+++ src/components/Elements.js
@@ -1,38 +1,3 @@
-// /**
-//  * @type {HTMLElement}
-//  */
-//
-// const overlay = document.getElementById('overlay');
-//
-// // Page title
-// const pageTitleElement = document.getElementById('page-title');
-// const pageTitle = 'Pomodoro App';
-//
-// // Favicon
-// const faviconElement = document.getElementById('favicon');
-//
-// // Task title
-// const taskTitleInput = document.getElementById('task-title-input');
-//
-// // Timer element and buttons
-// const timerElement = document.getElementById('timer');
-// const sessionStatus = document.getElementById('session-status');
-// const startButton = document.getElementById('start');
-// const stopButton = document.getElementById('stop');
-//
-// // Timer settings button and values
-// const openSettings = document.getElementById('openSettings');
-// const closeSettings = document.getElementById('closeSettings');
-// const applySettingsButton = document.getElementById('timer-settings-apply');
-// const workTime = document.getElementById('work-time');
-// const shortBreak = document.getElementById('short-break');
-// const longBreak = document.getElementById('long-break');
-// const longBreakInterval = document.getElementById('long-break-interval');
-// const settingsModal = document.getElementById('settings-container');
-
-
-//////////////
-
 export default {
   /**
    * @type {HTMLElement}
@@ -75,4 +40,3 @@ export default {
   audioEndBreak: document.getElementById('end-break'),
 
 }
-
